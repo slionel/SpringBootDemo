cc84def92a3da937b2f192ec809cbabb7c684c21
azusa <411596028@qq.com>
2019-08-17 17:04:50 +0800

    增加分页显示 修改传值时清空currentPages

--- SpringBootJPA/src/main/resources/static/js/page.js
+++ SpringBootJPA/src/main/resources/static/js/page.js
@@ -19,36 +19,41 @@ $(document).ready(function(){
 
             var totalPages = json.totalPages;
             $(".pagination").empty();
-            $(".pagination").append('<li class="page-item active"><a class="page-link" href="#" id="previouspage">Previous</a></li>');
-
+            $(".pagination").append('<li class=""><a class="page-link" href="#" id="firstpage">首页</a></li>');
+            $(".pagination").append('<li class=""><a class="page-link" href="#" id="previouspage">上一页</a></li>');
             for(var j = 0; j < totalPages; j++){
                 $(".pagination").append(
                     '<li class="page-item" id="pageno'+j+'"><a class="page-link" href="#">'+(j+1)+'</a></li>'
                 );
             }
+            $(".pagination").append('<li class=""><a class="page-link" href="#" id="nextpage">下一页</a></li>');
+            $(".pagination").append('<li class=""><a class="page-link" href="#" id="lastpage">尾页</a></li>');
 
-            $(".pagination").append('<li class="page-item"><a class="page-link" href="#" id="nextpage">Next</a></li>');
 
+            //显示当前页数
             $(".page-item").removeClass("active");
             $("#pageno"+currentPage).addClass("active");
 
 
             //下一页
             $("#nextpage").click(function () {
-                if(currentPage < totalPages){
-                    currentPage = currentPage + 1;
+                if(currentPage < totalPages-1){
+                    var curr = new Number(currentPage);
+                    currentPage = curr+1;
                     getData();
                 }
                 else{
                     alert("这是最后一页");
                 }
+
             });
 
 
             //上一页
             $("#previouspage").click(function () {
                 if(currentPage > 0){
-                    currentPage = currentPage - 1;
+                    var curr = new Number(currentPage);
+                    currentPage = curr-1;
                     getData();
                 }
                 else{
@@ -62,6 +67,19 @@ $(document).ready(function(){
                 currentPage = idval.substr(6);
                 getData();
             });
+
+            $("#firstpage").click(function () {
+                currentPage = 0;
+                getData();
+            });
+
+
+            $("#lastpage").click(function () {
+                currentPage = totalPages-1;
+                getData();
+            });
+
+
 
         });
     }
